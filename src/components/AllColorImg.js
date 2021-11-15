2727ee5e5dba9743cd31f8138b36b86933a185aa
Skye Zhang <skyesherly@gmail.com>
2021-11-15 13:50:35 +1100

    finished

--- src/components/AllColorImg.js
+++ src/components/AllColorImg.js
@@ -1,18 +1,23 @@
 import React from "react";
 
 function AllColorImg() {
-  const rgbRange = [];
-  for (let i = 1; i < 256; i += 8) {
-    rgbRange.push(i);
+  //Get R, G, B values
+  function rgbRange(start) {
+    const r = [];
+    for (let i = start; i < 256; i += 8) {
+      r.push(i);
+    }
+    return r;
   }
-  const rgbData = cartesian(rgbRange, rgbRange, rgbRange); //all the color value array
-  function cartesian(...args) {
-    var r = [],
-      max = args.length - 1;
+
+  //Get all the rgb colors
+  function cartesian(...array) {
+    const r = [],
+      max = array.length - 1;
     function helper(arr, i) {
-      for (var j = 0, l = args[i].length; j < l; j++) {
-        var a = arr.slice(0);
-        a.push(args[i][j]);
+      for (let j = 0, l = array[i].length; j < l; j++) {
+        const a = arr.slice(0);
+        a.push(array[i][j]);
         if (i === max) r.push(a);
         else helper(a, i + 1);
       }
@@ -20,18 +25,38 @@ function AllColorImg() {
     helper([], 0);
     return r;
   }
+
+  // shuffle color array
+  function shuffle(array) {
+    const r = array.slice(0);
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [r[i], r[j]] = [r[j], r[i]];
+    }
+    return r;
+  }
+  const rgbData = cartesian(rgbRange(1), rgbRange(2), rgbRange(3)); //all the color value array
+  const shufflergbData = shuffle(rgbData); //shuffled array
+
   return (
-    <div style={{ width: "256px", height: "128px" }}>
-      {rgbData.map((colour, index) => {
+    <div
+      style={{
+        width: "50%",
+        margin: " 40px  auto",
+        display: "flex",
+        flexWrap: "wrap",
+      }}
+    >
+      {shufflergbData.map((colour, index) => {
         return (
           <div
             key={index}
             id={index}
             style={{
               backgroundColor: `rgb(${colour})`,
-              width: "1px",
-              height: "1px",
-              display: "inline-block",
+              width: "3px",
+              height: "3px",
+              margin: "1px",
             }}
           ></div>
         );
